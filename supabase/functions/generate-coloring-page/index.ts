f3d41cab308f05f285ad3361492e81e3e5ad504a
grantashl1-commits <grantashl1@gmail.com>
2026-08-16 19:54:20 +1200

    generate-coloring-page: fix CORS preflight (allow apikey + x-client-info headers)

--- supabase/functions/generate-coloring-page/index.ts
+++ supabase/functions/generate-coloring-page/index.ts
@@ -61,7 +61,7 @@ serve(async (req: Request) => {
       headers: {
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods": "POST, OPTIONS",
-        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        "Access-Control-Allow-Headers": "authorization, x-client-info, apikey, content-type",
       },
     });
   }
